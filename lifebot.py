4f082f6e6e37b7da507914bb3ba0caaef2455731
Charlie Windolf <cwindolf95@gmail.com>
2016-04-03 14:11:22 -0400

    roadmap

--- lifebot.py
+++ lifebot.py
@@ -1,9 +1,23 @@
+ # -*- coding: utf-8 -*-
 import tweepy
-from os import environ
+import pickle
+from env import CONSUMER_TOKEN, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
 
 
 
 
 
 if __name__ == "main":
-	# authenticate ******************************
+	# authenticate ********************************************************** #
+	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	api = tweepy.API(auth)
+
+	# initialize, or load state if there is one ***************************** #
+
+
+	# generate next state *************************************************** #
+
+
+	# tweet it ************************************************************** #
+	api.update_status()
